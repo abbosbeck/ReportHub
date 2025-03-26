2c5cde7f4bee1a05a6bf81fdf431d0a6c893385d
vladimir-semeryuk <vladimir.semeryuk@gmail.com>
2025-03-26 13:36:11 +0500

    Create base domain entities, add user entity

---
+++ src/Domain/Common/ISoftDeletable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common;
+public interface ISoftDeletable
+{
+    public bool IsDeleted { get; set; }
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Entities;
+public class User : BaseAuditableEntity, ISoftDeletable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsDeleted { get; set; }
+}
