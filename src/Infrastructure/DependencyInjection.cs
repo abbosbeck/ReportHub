6af8f86207230c699b038cfacb2d5fd938339717
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-03-27 16:28:21 +0500

    Add Key Vault

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -6,13 +6,15 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Persistence.Interceptors;
 using Infrastructure.Persistence.Repositories;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var connectionString = GetConnectionString(configuration);
 
         services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
         services.AddScoped<ISaveChangesInterceptor, SoftDeletableInterceptor>();
@@ -29,5 +31,21 @@ public static class DependencyInjection
         services.AddScoped<IUserRepository, UserRepository>();
 
         return services;
+    }
+
+    private static string GetConnectionString(IConfiguration configuration)
+    {
+        var keyVaultURL = configuration.GetSection("KeyVault:KeyVaultURL");
+        var keyVaultClientId = configuration.GetSection("KeyVault:ClientId");
+        var keyVaultClientSecret = configuration.GetSection("KeyVault:ClientSecret");
+        var keyVaultDirectoryID = configuration.GetSection("KeyVault:DirectoryID");
+        
+        var credential = new ClientSecretCredential(
+                    keyVaultDirectoryID.Value!.ToString(),
+                    keyVaultClientId.Value!.ToString(),
+                    keyVaultClientSecret.Value!.ToString());
+        var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), credential);
+        
+        return client.GetSecret("DatabaseConnectionString").Value.Value.ToString();
     }
 }
