00a9151c050d7a66d0383dce15d6ba8684994be5
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-03-30 16:42:53 +0500

    Test If CI/CD Works

--- src/Api/Controllers/UsersController.cs
+++ src/Api/Controllers/UsersController.cs
@@ -18,4 +18,10 @@ public class UsersController(ISender mediator) : ApiControllerBase(mediator)
 
         return Ok(result);
     }
+
+    [HttpGet]
+    public IActionResult GetSomethingToTest()
+    {
+        return Ok("Working");
+    }
 }
