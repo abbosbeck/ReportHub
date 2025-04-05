33ef2490e4fb2ef864e0d1970daa96e48f600454
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-05 22:06:53 +0500

    Implement Give User Role Handler

---
+++ src/Application/Users/GiveRoleToUser/GiveRoleToUserHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entity;
+
+namespace Application.Users.GiveRoleToUser
+{
+    public sealed record GiveRoleToUserCommand(Guid userId, string roleName) : IRequest<bool>;
+
+    public class GiveRoleToUserHandler(
+        IUserRepository userRepository,
+        IUserRoleRepository userRoleRepository)
+        : IRequestHandler<GiveRoleToUserCommand, bool>
+    {
+        public async Task<bool> Handle(GiveRoleToUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await userRepository.GetUserByIdAsync(request.userId);
+            if (user == null)
+            {
+                throw new ArgumentNullException("User not found!");
+            }
+
+            var userRole = new UserRole
+            {
+                UserId = user.Id,
+                Role = new Role
+                {
+                    Name = request.roleName,
+                },
+            };
+
+            var addUserRole = await userRoleRepository.GiveRoleToUserAsync(userRole);
+
+            return addUserRole;
+        }
+    }
+}
