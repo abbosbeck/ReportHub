922afd9130e71b38931ef193c83d0084b4915b02
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-10 21:17:30 +0500

    Add Client Member Endpoint

---
+++ src/Application/Common/Constants/ClientUserRoles.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Common.Constants;
+
+public static class ClientUserRoles
+{
+    public const string ClientAdmin = "ClientAdmin";
+    public const string Regular = "Regular";
+}
