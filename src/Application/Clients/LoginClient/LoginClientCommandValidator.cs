b5845443ec4c8f867ef8d5ce02205a830596ef51
Umidjon <makhammadsoliyev@gmail.com>
2025-04-10 23:41:13 +0500

    Add LoginClient.cs, LoginClientCommandValidator.cs, LoginDto.cs

---
+++ src/Application/Clients/LoginClient/LoginClientCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Clients.LoginClient;
+
+public class LoginClientCommandValidator : AbstractValidator<LoginClientCommand>
+{
+    public LoginClientCommandValidator()
+    {
+        RuleFor(u => u.Password).NotEmpty();
+        RuleFor(u => u.Email).EmailAddress().WithMessage("Please provide valid email");
+    }
+}
---
+++ src/Application/Clients/LoginClient/LoginDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Clients.LoginClient;
+
+public class LoginDto
+{
+    public string AccessToken { get; set; }
+}
