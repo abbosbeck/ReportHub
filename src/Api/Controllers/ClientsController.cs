d7f553ffede855b08f7e6795fadd0dc61e63edc1
Umidjon <makhammadsoliyev@gmail.com>
2025-04-10 23:47:23 +0500

    Merge

--- src/Api/Controllers/ClientsController.cs
+++ src/Api/Controllers/ClientsController.cs
@@ -1,5 +1,8 @@
-﻿using Application.Clients.LoginClient;
+﻿using Application.Clients.AddClientMember;
+using Application.Clients.GiveRoleToClientMember;
+using Application.Clients.LoginClient;
 using Application.Clients.RegisterClient;
+using Application.Clients.SoftDeleteClient;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +22,30 @@ public class ClientsController(ISender mediator) : ApiControllerBase(mediator)
     public async Task<IActionResult> LoginAsync([FromBody] LoginClientCommand command)
     {
         var result = await Mediator.Send(command);
+
+        return Ok(result);
+    }
+
+    [HttpPost("add-client-member")]
+    public async Task<IActionResult> AddClientMember([FromBody] AddClientMemberCommand addClientMemberCommand)
+    {
+        var result = await Mediator.Send(addClientMemberCommand);
+
+        return Ok(result);
+    }
+
+    [HttpPost("give-role")]
+    public async Task<IActionResult> GiveRoleToClientMember([FromBody] GiveRoleToClientMemberCommand giveRoleToClientMemberCommand)
+    {
+        var result = await Mediator.Send(giveRoleToClientMemberCommand);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("soft-delete-client")]
+    public async Task<IActionResult> SoftDeleteClient(Guid clientId)
+    {
+        var result = await Mediator.Send(new SoftDeleteClientCommand { ClientId = clientId });
 
         return Ok(result);
     }
