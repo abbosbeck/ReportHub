b22a75f1fa4ae96ff80f0f9c5207d657b0af3ede
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-10 18:45:36 +0500

    Add Custom Client Role Attirbute

---
+++ src/Application/Common/Attributes/RequiresSystemRoleAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class RequiresSystemRoleAttribute(params string[] systemRoles) : Attribute
+{
+    public string[] SystemRoles { get; init; } = systemRoles;
+}
