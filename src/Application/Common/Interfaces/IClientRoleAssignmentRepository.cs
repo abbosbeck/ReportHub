51c91375f6ea647a91e73775ea705278371325b9
Umidjon <makhammadsoliyev@gmail.com>
2025-04-10 23:42:02 +0500

    Add ClientRoleAssignmentRepository.cs, IClientRoleAssignmentRepository.cs

---
+++ src/Application/Common/Interfaces/IClientRoleAssignmentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Common.Interfaces;
+
+public interface IClientRoleAssignmentRepository
+{
+    Task<List<string>> GetClientRolesByClientIdAsync(Guid clientId);
+}
---
+++ src/Infrastructure/Persistence/Repositories/ClientRoleAssignmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Interfaces;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class ClientRoleAssignmentRepository(AppDbContext dbContext) : IClientRoleAssignmentRepository
+{
+    public async Task<List<string>> GetClientRolesByClientIdAsync(Guid clientId)
+    {
+        return await dbContext.ClientRoleAssignment
+            .Where(t => t.ClientId == clientId)
+            .Select(t => t.ClientRole.Name)
+            .ToListAsync();
+    }
+}
