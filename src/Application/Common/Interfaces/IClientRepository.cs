91f60117f4c091722acd9d5f335cbd1d79103aa8
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-10 14:53:16 +0500

    Create Register Client Repository

---
+++ src/Application/Common/Interfaces/IClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces;
+
+public interface IClientRepository
+{
+    Task<Client> AddClientAdminAsync(Client client, Guid userId);
+
+    Task<Client> GetClientByEmailAsync(string email);
+
+    Task<Client> GetClientByIdAsync(Guid Id);
+}
---
+++ src/Infrastructure/Persistence/Repositories/ClientRepository.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class ClientRepository(AppDbContext context) : IClientRepository
+{
+    public async Task<Client> AddClientAdminAsync(Client client, Guid userId)
+    {
+        client.Id = Guid.NewGuid();
+
+        await context.Clients.AddAsync(client);
+        await RegisterClientRoleAssignmentAsync(client.Id, userId);
+
+        await context.SaveChangesAsync();
+
+        return await GetClientByIdAsync(client.Id);
+    }
+
+    public async Task<Client> GetClientByEmailAsync(string email)
+    {
+        return await context.Clients.FirstOrDefaultAsync(c => c.Email == email);
+    }
+
+    public async Task<Client> GetClientByIdAsync(Guid Id)
+    {
+        return await context.Clients.FirstOrDefaultAsync(c => c.Id == Id);
+    }
+
+    private async Task RegisterClientRoleAssignmentAsync(Guid clientId, Guid userId)
+    {
+        var clientRole = await context.ClientRoles
+            .FirstOrDefaultAsync(r => r.Name == "ClientAdmin");
+
+        var clientRoleAssignment = new ClientRoleAssignment
+        {
+            UserId = userId,
+            ClientRoleId = clientRole.Id,
+            ClientId = clientId,
+        };
+        await context.ClientRoleAssignment.AddAsync(clientRoleAssignment);
+    }
+}
