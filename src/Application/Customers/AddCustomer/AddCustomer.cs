457df1d4ad3d9729b67f5758bf73903ef89e3f51
Umidjon <makhammadsoliyev@gmail.com>
2025-04-11 12:05:15 +0500

    Add AddCustomer.cs, AddCustomerCommandValidator.cs, CustomerDto.cs, CustomerProfile.cs

---
+++ src/Application/Customers/AddCustomer/AddCustomer.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Application.Customers.AddCustomer;
+
+public class AddCustomerCommand : IRequest<CustomerDto>
+{
+    public string Name { get; init; }
+
+    public string Email { get; init; }
+
+    public string Country { get; init; }
+
+    public Guid ClientId { get; init; }
+}
+
+public class AddCustomerCommandHandler(
+    IMapper mapper,
+    ICustomerRepository repository,
+    IValidator<AddCustomerCommand> validator)
+    : IRequestHandler<AddCustomerCommand, CustomerDto>
+{
+    public async Task<CustomerDto> Handle(AddCustomerCommand request, CancellationToken cancellationToken)
+    {
+        var customer = mapper.Map<Customer>(request);
+        var result = await repository.AddAsync(customer);
+
+        return mapper.Map<CustomerDto>(result);
+    }
+}
---
+++ src/Application/Customers/AddCustomer/CustomerProfile.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Customers.AddCustomer;
+
+public class CustomerProfile : Profile
+{
+    public CustomerProfile()
+    {
+        CreateMap<AddCustomerCommand, Customer>();
+        CreateMap<Customer, CustomerDto>();
+    }
+}
