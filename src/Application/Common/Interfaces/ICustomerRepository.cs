8cc6958a90bbc3dade9f5a7acc8d0296312b60ba
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-11 12:00:48 +0500

    Create Customer Repository

---
+++ src/Application/Common/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces;
+
+public interface ICustomerRepository
+{
+    Task AddCustomerAsync(Customer customer);
+
+    Task<Customer> GetCustomerById(Guid id);
+
+    Task<Customer> GetCustomerByEmail(string email);
+
+    bool CheckIsCustomerExistByEmail(string email);
+
+    Task<IEnumerable<Customer>> GetAllCustomer();
+
+    Task UpdateCustomerAsync(Guid id);
+
+    Task SaveChangesAsync();
+}
---
+++ src/Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using System.Runtime.Serialization;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class CustomerRepository(AppDbContext context) : ICustomerRepository
+{
+    public async Task AddCustomerAsync(Customer customer)
+    {
+        await context.Customers.AddAsync(customer);
+        await SaveChangesAsync();
+    }
+
+    public Task<IEnumerable<Customer>> GetAllCustomer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer> GetCustomerById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CheckIsCustomerExistByEmail(string email)
+    {
+        return context.Customers.Any(c => c.Email == email);
+    }
+
+    public async Task<Customer> GetCustomerByEmail(string email)
+    {
+        var customer = await context.Customers.FirstOrDefaultAsync(c => c.Email == email);
+
+        return customer;
+    }
+
+    public Task UpdateCustomerAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
