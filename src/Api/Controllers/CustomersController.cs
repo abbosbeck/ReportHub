5f136d201fb1f7d674e5ffb95b2eba002e94303f
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-11 12:01:29 +0500

    Add CreateCustomer Endpoint

---
+++ src/Api/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Customers.CreateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class CustomersController(ISender mediator) : ApiControllerBase(mediator)
+{
+    [HttpPost("create-customer")]
+    public async Task<IActionResult> CreateCustomer([FromBody] CreateCustomerCommand command)
+    {
+        var result = await Mediator.Send(command);
+
+        return Ok(result);
+    }
+}
