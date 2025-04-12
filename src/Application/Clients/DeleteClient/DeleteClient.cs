4d0536e95392f0950b08367dc91997c55fb32a38
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-12 20:40:30 +0500

    Implement DeleteClient

---
+++ src/Application/Clients/DeleteClient/DeleteClient.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.Clients.DeleteClient;
+
+public class DeleteClientCommand : IRequest<bool>
+{
+    public Guid ClientId { get; set; }
+}
+
+public class DeleteClientCommandHandler(
+    IClientRepository clientRepository,
+    IValidator<DeleteClientCommand> validator)
+    : IRequestHandler<DeleteClientCommand, bool>
+{
+    public async Task<bool> Handle(DeleteClientCommand request, CancellationToken cancellationToken)
+    {
+        await validator.ValidateAndThrowAsync(request, cancellationToken);
+
+        var client = await clientRepository.GetByIdAsync(request.ClientId)
+            ?? throw new NotFoundException($"Client is not found with this id: {request.ClientId}");
+
+        client.IsDeleted = true;
+        client.DeletedOn = DateTime.UtcNow;
+        await clientRepository.UpdateAsync(client);
+
+        return true;
+    }
+}
