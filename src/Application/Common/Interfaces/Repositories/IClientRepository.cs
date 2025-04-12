00732c119a51a9581f47ea5ca6a6dfbbf4a5a7f9
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-12 14:34:14 +0500

    Add Client Repository

---
+++ src/Application/Common/Interfaces/Repositories/IClientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces.Repositories;
+
+public interface IClientRepository
+{
+    Task<Client> GetByIdAsync(Guid clientId);
+
+    Task<Client> AddAsync(Client client);
+
+    Task<Client> UpdateAsync(Client client);
+
+    Task<bool> DeleteAsync(Client client);
+}
