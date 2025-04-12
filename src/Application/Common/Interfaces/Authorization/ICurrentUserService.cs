4270b0016a1d764cb4bfa20db859e561e9cdcc4e
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-12 22:38:17 +0500

    Get Client Roles in User Service

--- src/Application/Common/Interfaces/Authorization/ICurrentUserService.cs
+++ src/Application/Common/Interfaces/Authorization/ICurrentUserService.cs
@@ -1,8 +1,12 @@
-﻿namespace Application.Common.Interfaces.Authorization;
+﻿using Application.Common.Services;
+
+namespace Application.Common.Interfaces.Authorization;
 
 public interface ICurrentUserService
 {
     Guid UserId { get; }
 
     List<string> Roles { get; }
+
+    List<JwtClientRole> ClientRoles();
 }
--- src/Application/Common/Services/CurrentUserService.cs
+++ src/Application/Common/Services/CurrentUserService.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Common.Exceptions;
 using Application.Common.Interfaces.Authorization;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Application.Common.Services;
 
@@ -10,4 +11,12 @@ public class CurrentUserService(IHttpContextAccessor httpContextAccessor) : ICur
                           throw new ForbiddenException("User Context is unavailable");
 
     public List<string> Roles => httpContextAccessor.HttpContext?.User.GetRoles() ?? new List<string> { };
+
+    public List<JwtClientRole> ClientRoles()
+    {
+        var clientRolesJson = httpContextAccessor.HttpContext?.User.FindFirst("ClientRoles")?.Value;
+        var clientRoles = JsonConvert.DeserializeObject<List<JwtClientRole>>(clientRolesJson ?? string.Empty);
+
+        return clientRoles;
+    }
 }
