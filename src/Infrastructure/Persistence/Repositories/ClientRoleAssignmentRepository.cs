ce15229a909df0681834a282fb891e35895585c2
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-12 14:37:24 +0500

    Add Client Role Assignment Repositories

---
+++ src/Infrastructure/Persistence/Repositories/ClientRoleAssignmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class ClientRoleAssignmentRepository(AppDbContext context) : IClientRoleAssignmentRepository
+{
+    public async Task AddAsync(ClientRoleAssignment clientRoleAssignment)
+    {
+        await context.AddAsync(clientRoleAssignment);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/ClientRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class ClientRoleRepository(AppDbContext context) : IClientRoleRepository
+{
+    public async Task<ClientRole> GetByNameAsync(string name)
+    {
+        return await context.ClientRoles.FirstOrDefaultAsync(cr => cr.Name == name);
+    }
+}
