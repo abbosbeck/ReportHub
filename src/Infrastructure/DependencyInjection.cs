2aa4d9cdcff00daff4e54eb5c4e75fc99e188288
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-12 14:24:41 +0500

    Add SystemRole Repository Injection

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -32,6 +32,7 @@ public static class DependencyInjection
         services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ISystemRoleAssignmentRepository, SystemRoleAssignmentRepository>();
+        services.AddScoped<ISystemRoleRepository, SystemRoleRepository>();
         services.AddScoped<IClientRepository, ClientRepository>();
         services.AddScoped<IClientRoleAssignmentRepository, ClientRoleAssignmentRepository>();
         services.AddScoped<IClientRoleRepository, ClientRoleRepository>();
