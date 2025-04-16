0b779f3bf192eecd4af36e55b871918e707cfa7c
Umidjon <makhammadsoliyev@gmail.com>
2025-04-17 02:03:57 +0500

    Add CountryApiService.cs, ICountryApiService.cs

---
+++ src/Infrastructure/External/CountryApiService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+using Application.Common.Interfaces.External;
+
+namespace Infrastructure.External;
+
+public class CountryApiService(HttpClient httpClient) : ICountryApiService
+{
+    public async Task<CountryDto> GetByCode(string countryCode)
+    {
+        var response = await httpClient.GetAsync($"alpha/{countryCode}");
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        return await response.Content.ReadFromJsonAsync<CountryDto>();
+    }
+}
