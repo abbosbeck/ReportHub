9f76fc63580a9239b4fca8eb04a05522e3827a98
Umidjon <makhammadsoliyev@gmail.com>
2025-04-17 02:06:56 +0500

    Add CountryApi

---
+++ src/Application/Common/Interfaces/External/CountryDto.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Application.Common.Interfaces.External;
+
+public class CountryDto
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("currencies")]
+    public List<CurrencyDto> Currencies { get; set; }
+
+    [JsonProperty("alpha3Code")]
+    public string ThreeLetterCode { get; set; }
+}
---
+++ src/Application/Common/Interfaces/External/ICountryApiService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Common.Interfaces.External;
+
+public interface ICountryApiService
+{
+    Task<CountryDto> GetByCode(string countryCode);
+}
