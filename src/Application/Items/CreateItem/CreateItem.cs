c88a6e936b21fbab2e4d6f7fc7ade5211420e3d8
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-04-18 01:27:08 +0500

    Add CreateClient Handler

---
+++ src/Application/Items/CreateItem/CreateItem.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Application.Items.CreateItem;
+
+public class CreateItemCommand : IRequest<ItemDto>, IClientRequest
+{
+    public string Name { get; init; }
+
+    public string Description { get; init; }
+
+    public decimal Price { get; init; }
+
+    public string CurrencyCode { get; init; }
+
+    public Guid ClientId { get; set; }
+
+    public Guid InvoiceId { get; init; }
+}
+
+public class CreateItemCommandHandler(
+    IItemRepository repository,
+    IMapper mapper,
+    IValidator<CreateItemCommand> validator)
+    : IRequestHandler<CreateItemCommand, ItemDto>
+{
+    public async Task<ItemDto> Handle(CreateItemCommand request, CancellationToken cancellationToken)
+    {
+        await validator.ValidateAndThrowAsync(request);
+
+        var newItem = mapper.Map<Item>(request);
+        var createdItem = await repository.AddAsync(newItem)
+            ?? throw new BadRequestException("Item creation failed!");
+
+        return mapper.Map<ItemDto>(createdItem);
+    }
+}
