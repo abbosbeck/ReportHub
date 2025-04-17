bdc47fa9f050dcdbfeff7a79ff7af864d07a32bc
Umidjon <makhammadsoliyev@gmail.com>
2025-04-17 10:04:31 +0500

    Add UpdateAsync And GetByIdAsync Method To CustomerRepository

--- src/Application/Common/Interfaces/Repositories/ICustomerRepository.cs
+++ src/Application/Common/Interfaces/Repositories/ICustomerRepository.cs
@@ -1,8 +1,13 @@
-﻿using Domain.Entities;
+﻿using Application.Customers.UpdateCustomer;
+using Domain.Entities;
 
 namespace Application.Common.Interfaces.Repositories;
 
 public interface ICustomerRepository
 {
     Task<Customer> AddAsync(Customer customer);
+
+    Task<Customer> UpdateAsync(Customer customer);
+
+    Task<Customer> GetByIdAsync(Guid id);
 }
--- src/Infrastructure/Persistence/Repositories/CustomerRepository.cs
+++ src/Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -12,4 +12,17 @@ public class CustomerRepository(AppDbContext context) : ICustomerRepository
 
         return customer;
     }
+
+    public async Task<Customer> UpdateAsync(Customer customer)
+    {
+        context.Update(customer);
+        await context.SaveChangesAsync();
+
+        return customer;
+    }
+
+    public async Task<Customer> GetByIdAsync(Guid id)
+    {
+        return await context.Customers.FindAsync(id);
+    }
 }
