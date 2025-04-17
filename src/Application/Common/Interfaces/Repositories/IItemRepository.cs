872c9494c75741957cd0cc6598732564650d6a1c
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-04-18 01:20:21 +0500

    Add Repository

---
+++ src/Application/Common/Interfaces/Repositories/IItemRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces.Repositories;
+
+public interface IItemRepository
+{
+    Task<Item> AddAsync(Item item);
+}
---
+++ src/Infrastructure/Persistence/Repositories/ItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class ItemRepository(AppDbContext context) : IItemRepository
+{
+    public async Task<Item> AddAsync(Item item)
+    {
+        await context.Items.AddAsync(item);
+        await context.SaveChangesAsync();
+
+        return item;
+    }
+}
