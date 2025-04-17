05c627c62ba6133a992a58d3752189af43786424
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-17 21:51:11 +0500

    Add Currency Exchange Stuff

---
+++ src/Application/Common/Interfaces/External/ICurrencyExchange.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common.Interfaces.External;
+
+public interface ICurrencyExchange
+{
+    Task<HistoricalExchangeRatesDto> GetHistoricalExchangeRates(string currency, DateTime time, decimal amount);
+
+    Task<decimal> ExchangeCurrency(string source, string destination, decimal amount, DateTime time);
+}
