f2d207050724defcff9c69224420cdf417a5d92f
Umidjon <makhammadsoliyev@gmail.com>
2025-04-17 10:05:38 +0500

    Add UpdateCustomer Endpoint

--- src/Api/Controllers/CustomersController.cs
+++ src/Api/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Customers.CreateCustomer;
+using Application.Customers.UpdateCustomer;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,15 @@ public class CustomersController(ISender mediator) : ApiControllerBase(mediator)
     [AllowAnonymous]
     [HttpPost]
     public async Task<IActionResult> CreateAsync([FromBody] CreateCustomerCommand command)
+    {
+        var result = await Mediator.Send(command);
+
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync([FromBody] UpdateCustomerCommand command)
     {
         var result = await Mediator.Send(command);
 
