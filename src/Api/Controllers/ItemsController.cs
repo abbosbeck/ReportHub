e21c08de2385b40b0526ac45fb5bf305b66ed97d
Abbos Abduqayumov <AbbosAlisherovich2003@gmail.com>
2025-04-18 01:40:57 +0500

    Add Endpoint

---
+++ src/Api/Controllers/ItemsController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Items.CreateItem;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class ItemsController(ISender mediator) : ApiControllerBase(mediator)
+{
+    [HttpPost("Create")]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateItemCommand command)
+    {
+        var result = await Mediator.Send(command);
+
+        return Ok(result);
+    }
+}
