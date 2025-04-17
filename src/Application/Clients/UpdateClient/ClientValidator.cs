8f038f71ecb571c8a75b709bb85dc19b8a1a1d00
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-17 11:35:50 +0500

    Create Update Client Handler

---
+++ src/Application/Clients/UpdateClient/ClientValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Clients.UpdateClient;
+
+public class ClientValidator : AbstractValidator<UpdateClientCommand>
+{
+    public ClientValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .MaximumLength(64);
+
+        RuleFor(c => c.ClientId)
+            .NotEmpty()
+            .Must(BeAValidGuid);
+    }
+
+    private static bool BeAValidGuid(Guid guid)
+    {
+        return Guid.TryParse(guid.ToString(), out _);
+    }
+}
---
+++ src/Application/Clients/UpdateClient/UpdateClient.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.Clients.UpdateClient;
+
+public class UpdateClientCommand : IRequest<ClientDto>, IClientRequest
+{
+    public Guid ClientId { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class UpdateClientCommandHandler(
+    IClientRepository repository,
+    IMapper mapper,
+    ICurrentUserService currentUser,
+    IValidator<UpdateClientCommand> validator)
+    : IRequestHandler<UpdateClientCommand, ClientDto>
+{
+    public async Task<ClientDto> Handle(UpdateClientCommand request, CancellationToken cancellationToken)
+    {
+        await validator.ValidateAndThrowAsync(request);
+
+        var client = await repository.GetByIdAsync(request.ClientId)
+            ?? throw new NotFoundException($"Client is not found with this id: {request.ClientId}");
+
+        client.Name = request.Name;
+        client.LastModifiedOn = DateTime.UtcNow;
+        client.LastModifiedBy = currentUser.UserId.ToString();
+
+        var updatedClient = await repository.UpdateAsync(client);
+
+        return mapper.Map<ClientDto>(updatedClient);
+    }
+}
