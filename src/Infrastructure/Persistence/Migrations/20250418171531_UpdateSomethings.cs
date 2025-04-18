9d057fd1080986d8a335d60cf76ea8cc2fd466ac
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 22:27:25 +0500

    Add Migration

---
+++ src/Infrastructure/Persistence/Migrations/20250418171531_UpdateSomethings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSomethings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Currency",
+                table: "Invoices",
+                newName: "CurrencyCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CurrencyCode",
+                table: "Invoices",
+                newName: "Currency");
+        }
+    }
+}
