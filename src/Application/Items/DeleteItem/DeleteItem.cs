0073d29cb477b7a62909cfc1bf21e6d2075e3878
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 10:19:13 +0500

    Add DeleteItem Handler

---
+++ src/Application/Items/DeleteItem/DeleteItem.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Attributes;
+using Application.Common.Constants;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.Items.DeleteItem;
+
+public class DeleteItemCommand : IRequest<bool>, IClientRequest
+{
+    public Guid ItemId { get; set; }
+
+    public Guid ClientId { get; set; }
+}
+
+[RequiresClientRole(ClientRoles.Owner, ClientRoles.ClientAdmin)]
+public class DeleteItemCommandHandler(IItemRepository repository) : IRequestHandler<DeleteItemCommand, bool>
+{
+    public async Task<bool> Handle(DeleteItemCommand request, CancellationToken cancellationToken)
+    {
+        var item = await repository.GetByIdAsync(request.ItemId)
+            ?? throw new NotFoundException($"Item is not found with this id: {request.ClientId}");
+
+        var isDeleted = await repository.DeleteAsync(item);
+
+        return isDeleted;
+    }
+}
