959e8b87339b4fe462336d3b63bee10c840947e9
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 20:53:43 +0500

    Fix Items

--- src/Application/Items/DeleteItem/DeleteItem.cs
+++ src/Application/Items/DeleteItem/DeleteItem.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using Application.Common.Attributes;
+﻿using Application.Common.Attributes;
 using Application.Common.Constants;
 using Application.Common.Exceptions;
 using Application.Common.Interfaces.Authorization;
