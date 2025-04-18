363775baa34e6db1c060141d9a7b39ce6bc1572e
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 09:52:53 +0500

    Add Global Query Filter For Client-Scoped Entities

--- src/Infrastructure/Persistence/Configurations/ClientConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/ClientConfiguration.cs
@@ -1,13 +1,15 @@
-﻿using Domain.Entities;
+﻿using Application.Common.Interfaces.Authorization;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Persistence.Configurations;
 
-internal class ClientConfiguration : IEntityTypeConfiguration<Client>
+internal class ClientConfiguration(IClientRequest clientRequest) : IEntityTypeConfiguration<Client>
 {
     public void Configure(EntityTypeBuilder<Client> builder)
     {
         builder.HasQueryFilter(c => !c.IsDeleted);
+        builder.HasQueryFilter(c => c.Id == clientRequest.ClientId);
 
         builder.Property(c => c.Name)
             .HasMaxLength(200)
