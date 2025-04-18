36f0183730541d39620394abd5434087d8268c57
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 20:54:21 +0500

    Add CreateCustomerRequest

--- src/Application/Customers/CreateCustomer/CreateCustomer.cs
+++ src/Application/Customers/CreateCustomer/CreateCustomer.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json.Serialization;
-using Application.Common.Attributes;
+﻿using Application.Common.Attributes;
 using Application.Common.Constants;
 using Application.Common.Exceptions;
 using Application.Common.Interfaces.Authorization;
@@ -11,13 +10,14 @@ namespace Application.Customers.CreateCustomer;
 
 public class CreateCustomerCommand : IRequest<CustomerDto>, IClientRequest
 {
-    public string Name { get; init; }
+    public CreateCustomerCommand(Guid clientId, CreateCustomerRequest request)
+    {
+        ClientId = clientId;
+        Customer = request;
+    }
 
-    public string Email { get; init; }
+    public CreateCustomerRequest Customer { get; set; }
 
-    public string CountryCode { get; init; }
-
-    [JsonIgnore]
     public Guid ClientId { get; set; }
 }
 
@@ -27,21 +27,23 @@ public class CreateCustomerCommandHandler(
     IClientRepository clientRepository,
     ICountryApiService countryApiService,
     ICustomerRepository customerRepository,
-    IValidator<CreateCustomerCommand> validator)
+    IValidator<CreateCustomerRequest> validator)
     : IRequestHandler<CreateCustomerCommand, CustomerDto>
 {
     public async Task<CustomerDto> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
     {
-        await validator.ValidateAndThrowAsync(request, cancellationToken: cancellationToken);
+        await validator.ValidateAndThrowAsync(request.Customer, cancellationToken: cancellationToken);
 
-        _ = await countryApiService.GetByCode(request.CountryCode)
-            ?? throw new NotFoundException($"Country is not found with this code: {request.CountryCode}" +
+        _ = await countryApiService.GetByCode(request.Customer.CountryCode)
+            ?? throw new NotFoundException($"Country is not found with this code: {request.Customer.CountryCode}" +
                                            $"Look at this https://www.iban.com/country-codes");
 
         _ = await clientRepository.GetByIdAsync(request.ClientId)
             ?? throw new NotFoundException($"Client is not found with this id: {request.ClientId}");
 
-        var customer = mapper.Map<Customer>(request);
+        var customer = mapper.Map<Customer>(request.Customer);
+        customer.ClientId = request.ClientId;
+
         await customerRepository.AddAsync(customer);
 
         return mapper.Map<CustomerDto>(customer);
