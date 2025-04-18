094ad20d76a75eec4fe5f07e76e4b4db2fae8ca0
Umidjon <makhammadsoliyev@gmail.com>
2025-04-18 23:26:27 +0500

    Refactor CreateCustomer

--- src/Application/Customers/CreateCustomer/CreateCustomer.cs
+++ src/Application/Customers/CreateCustomer/CreateCustomer.cs
@@ -2,31 +2,25 @@
 using Application.Common.Constants;
 using Application.Common.Exceptions;
 using Application.Common.Interfaces.Authorization;
-using Application.Common.Interfaces.External;
 using Application.Common.Interfaces.External.Countries;
 using Application.Common.Interfaces.Repositories;
 using Domain.Entities;
 
 namespace Application.Customers.CreateCustomer;
 
-public class CreateCustomerCommand : IRequest<CustomerDto>, IClientRequest
+public class CreateCustomerCommand(Guid clientId, CreateCustomerRequest request)
+    : IRequest<CustomerDto>, IClientRequest
 {
-    public CreateCustomerCommand(Guid clientId, CreateCustomerRequest request)
-    {
-        ClientId = clientId;
-        Customer = request;
-    }
+    public CreateCustomerRequest Customer { get; set; } = request;
 
-    public CreateCustomerRequest Customer { get; set; }
-
-    public Guid ClientId { get; set; }
+    public Guid ClientId { get; set; } = clientId;
 }
 
 [RequiresClientRole(ClientRoles.Owner, ClientRoles.ClientAdmin, ClientRoles.Operator)]
 public class CreateCustomerCommandHandler(
     IMapper mapper,
-    IClientRepository clientRepository,
     ICountryService countryService,
+    IClientRepository clientRepository,
     ICustomerRepository customerRepository,
     IValidator<CreateCustomerRequest> validator)
     : IRequestHandler<CreateCustomerCommand, CustomerDto>
@@ -36,7 +30,7 @@ public class CreateCustomerCommandHandler(
         await validator.ValidateAndThrowAsync(request.Customer, cancellationToken: cancellationToken);
 
         _ = await countryService.GetByCode(request.Customer.CountryCode)
-            ?? throw new NotFoundException($"Country is not found with this code: {request.Customer.CountryCode}" +
+            ?? throw new NotFoundException($"Country is not found with this code: {request.Customer.CountryCode}. " +
                                            $"Look at this https://www.iban.com/country-codes");
 
         _ = await clientRepository.GetByIdAsync(request.ClientId)
