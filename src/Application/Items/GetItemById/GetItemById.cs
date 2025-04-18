b968e575566fac76bb465a37a94a324a338867f4
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 09:57:17 +0500

    Create GetItemById Handler

---
+++ src/Application/Items/GetItemById/GetItemById.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.Items.GetItemById;
+
+public class GetItemByIdQuery : IRequest<ItemDto>, IClientRequest
+{
+    public Guid ClientId { get; set; }
+
+    public Guid ItemId { get; set; }
+}
+
+public class GetItemByIdQueryHandler(
+    IItemRepository repository,
+    IMapper mapper)
+    : IRequestHandler<GetItemByIdQuery, ItemDto>
+{
+    public async Task<ItemDto> Handle(GetItemByIdQuery request, CancellationToken cancellationToken)
+    {
+        var item = await repository.GetByIdAsync(request.ItemId)
+            ?? throw new NotFoundException($"Item is not found with this id: {request.ItemId}");
+
+        return mapper.Map<ItemDto>(item);
+    }
+}
---
+++ src/Application/Items/GetItemById/ItemDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Items.GetItemById;
+
+public class ItemDto
+{
+    public string Name { get; init; }
+
+    public string Description { get; init; }
+
+    public decimal Price { get; init; }
+
+    public string CurrencyCode { get; init; }
+
+    public Guid ClientId { get; init; }
+
+    public Guid InvoiceId { get; init; }
+}
