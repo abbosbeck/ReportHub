d5ee81a4372ffa688929b944b55a6f715982c4ea
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-18 09:45:22 +0500

    Create Item Repository

--- src/Application/Common/Interfaces/Repositories/IItemRepository.cs
+++ src/Application/Common/Interfaces/Repositories/IItemRepository.cs
@@ -4,5 +4,11 @@ namespace Application.Common.Interfaces.Repositories;
 
 public interface IItemRepository
 {
+    Task<Item> GetByIdAsync(Guid itemId);
+
     Task<Item> AddAsync(Item item);
+
+    Task<Item> UpdateAsync(Item item);
+
+    Task<bool> DeleteAsync(Item item);
 }
--- src/Infrastructure/Persistence/Repositories/ItemRepository.cs
+++ src/Infrastructure/Persistence/Repositories/ItemRepository.cs
@@ -7,7 +7,27 @@ public class ItemRepository(AppDbContext context) : IItemRepository
 {
     public async Task<Item> AddAsync(Item item)
     {
-        await context.Items.AddAsync(item);
+        await context.AddAsync(item);
+        await context.SaveChangesAsync();
+
+        return item;
+    }
+
+    public async Task<bool> DeleteAsync(Item item)
+    {
+        context.Remove(item);
+
+        return await context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Item> GetByIdAsync(Guid itemId)
+    {
+        return await context.Items.FindAsync(itemId);
+    }
+
+    public async Task<Item> UpdateAsync(Item item)
+    {
+        context.Update(item);
         await context.SaveChangesAsync();
 
         return item;
