fa4c50c0464dfd8f22b9c964363d1bd66ca144e9
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-19 22:42:49 +0500

    Add Update Plan Handler

---
+++ src/Application/Plans/UpdatePlan/UpdatePlan.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.Plans.UpdatePlan;
+
+public class UpdatePlanCommand(Guid clientId, UpdatePlanRequest request) : IRequest<PlanDto>, IClientRequest
+{
+    public UpdatePlanRequest Plan { get; set; } = request;
+
+    public Guid ClientId { get; set; } = clientId;
+}
+
+public class UpdatePlanCommandHandler(
+    IPlanRepository repository,
+    IValidator<UpdatePlanRequest> validator,
+    IMapper mapper)
+    : IRequestHandler<UpdatePlanCommand, PlanDto>
+{
+    public async Task<PlanDto> Handle(UpdatePlanCommand request, CancellationToken cancellationToken)
+    {
+        await validator.ValidateAndThrowAsync(request.Plan, cancellationToken);
+
+        var plan = await repository.GetByIdAsync(request.Plan.Id)
+            ?? throw new NotFoundException($"Plan is not found with this id: {request.ClientId}");
+        
+        mapper.Map(request.Plan, plan);
+
+        var updatedPlan = await repository.UpdateAsync(plan);
+
+        return mapper.Map<PlanDto>(updatedPlan);
+    }
+}
