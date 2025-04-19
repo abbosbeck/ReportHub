d3a928e45c19023b822d0f419e30e0e84c5f2fb3
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-19 21:41:56 +0500

    Implement Get Plan By Id Handler

---
+++ src/Application/Plans/GetPlanById/GetPlanById.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Application.Plans.GetPlanById;
+
+public class GetPlanByIdQuery : IRequest<PlanDto>, IClientRequest
+{
+    public Guid Id { get; set; }
+
+    public Guid ClientId { get; set; }
+}
+
+public class GetPlanByIdQueryHandler(
+    IPlanRepository planRepository,
+    IPlanItemRepository planItemRepository,
+    IItemRepository itemRepository,
+    IMapper mapper)
+    : IRequestHandler<GetPlanByIdQuery, PlanDto>
+{
+    public async Task<PlanDto> Handle(GetPlanByIdQuery request, CancellationToken cancellationToken)
+    {
+        var plan = await planRepository.GetByIdAsync(request.Id)
+            ?? throw new NotFoundException($"Plan is not found with this id: {request.ClientId}");
+
+        var itemIds = await planItemRepository.GetItemIdsByPlanIdAsync(plan.Id);
+
+        var items = await itemRepository.GetByIdsAsync(itemIds);
+        plan.Items = (ICollection<Item>)items;
+
+        return mapper.Map<PlanDto>(plan);
+    }
+}
---
+++ src/Application/Plans/GetPlanById/PlanProfile.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Application.Plans.GetPlanById;
+
+public class PlanProfile : Profile
+{
+    public PlanProfile()
+    {
+        CreateMap<Plan, PlanDto>()
+            .ForMember(p => p.Items, opt => opt.MapFrom(src => src.Items));
+
+        CreateMap<Item, ItemDto>();
+    }
+}
