af3f710b49ae73c58d9afc16b27e23a3ca9e7df0
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-19 21:43:00 +0500

    Add GetById Endpoint

--- src/Api/Controllers/PlansController.cs
+++ src/Api/Controllers/PlansController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Plans.CreatePlan;
+using Application.Plans.GetPlanById;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,15 @@ public class PlansController(ISender mediator) : ApiControllerBase(mediator)
     public async Task<IActionResult> CreateAsync([FromRoute] Guid clientId, [FromBody] CreatePlanRequest request)
     {
         var result = await Mediator.Send(new CreatePlanCommand(clientId, request));
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] Guid clientId, [FromRoute] Guid id)
+    {
+        var result = await Mediator.Send(new GetPlanByIdQuery { ClientId = clientId, Id = id });
 
         return Ok(result);
     }
