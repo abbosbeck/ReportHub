3764a36368e1563bb2f53216e79e9b8bd5213496
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-19 23:21:00 +0500

    Add Roles

--- src/Application/Plans/GetPlansList/GetPlansList.cs
+++ src/Application/Plans/GetPlansList/GetPlansList.cs
@@ -1,4 +1,6 @@
-﻿using Application.Common.Interfaces.Authorization;
+﻿using Application.Common.Attributes;
+using Application.Common.Constants;
+using Application.Common.Interfaces.Authorization;
 using Application.Common.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +11,7 @@ public class GetPlansListQuery : IRequest<List<PlanDto>>, IClientRequest
     public Guid ClientId { get; set; }
 }
 
+[RequiresClientRole(ClientRoles.Owner, ClientRoles.ClientAdmin)]
 public class GetPlansListQueryHandler(
     IPlanRepository repository,
     IMapper mapper)
