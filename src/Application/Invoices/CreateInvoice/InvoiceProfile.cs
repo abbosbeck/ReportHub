0699478c6ad23aeca77b56a8720b579c3062aca9
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-19 10:24:09 +0500

    Fix Naming

--- src/Application/Invoices/CreateInvoice/InvoiceProfile.cs
+++ src/Application/Invoices/CreateInvoice/InvoiceProfile.cs
@@ -7,10 +7,10 @@ public class InvoiceProfile : Profile
     public InvoiceProfile()
     {
         CreateMap<Invoice, InvoiceDto>()
-            .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items));
+            .ForMember(i => i.Items, opt => opt.MapFrom(src => src.Items));
         CreateMap<Item, ItemDto>();
         CreateMap<CreateInvoiceRequest, Invoice>()
-            .ForMember(x => x.Items, opt => opt.Ignore());
+            .ForMember(i => i.Items, opt => opt.Ignore());
 
         CreateMap<ItemRequestDto, Item>();
     }
