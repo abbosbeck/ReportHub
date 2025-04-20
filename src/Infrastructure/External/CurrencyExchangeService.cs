6f20e284ea22abc1677649595c78a22515fdc358
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-20 14:32:20 +0500

    Add User-Friendly Error Messages

--- src/Infrastructure/External/CurrencyExchangeService.cs
+++ src/Infrastructure/External/CurrencyExchangeService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using Application.Common.Exceptions;
 using Application.Common.Interfaces.External.CurrencyExchange;
 
 namespace Infrastructure.External;
@@ -25,8 +26,24 @@ public class CurrencyExchangeService(HttpClient httpClient) : ICurrencyExchangeS
     public async Task<decimal> ExchangeCurrencyAsync(string source, string destination, decimal amount, DateTime time)
     {
         var currency = await GetHistoricalExchangeRatesAsync(source, time, amount);
+
+        ValidateCurrencyCode(currency, source, destination);
+
         var exchangedCurrency = currency.ConversionAmounts[destination];
 
         return (decimal)exchangedCurrency;
+    }
+
+    private static void ValidateCurrencyCode(HistoricalExchangeRatesDto currency, string source, string destination)
+    {
+        if (!currency.ConversionAmounts.ContainsKey(source))
+        {
+            throw new BadRequestException($"{source} is not supported by the given API");
+        }
+
+        if (!currency.ConversionAmounts.ContainsKey(destination))
+        {
+            throw new BadRequestException($"{destination} is not supported by the given API");
+        }
     }
 }
