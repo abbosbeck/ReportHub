d20bd622659950aacf5b60685bbabb38851e4f5c
Umidjon <makhammadsoliyev@gmail.com>
2025-04-20 11:12:09 +0500

    Refactor ApplySoftDeleteQueryFilter Extension Method

--- src/Infrastructure/Persistence/Extensions/SoftDeleteModelBuilderExtensions.cs
+++ src/Infrastructure/Persistence/Extensions/SoftDeleteModelBuilderExtensions.cs
@@ -7,13 +7,13 @@ public static class SoftDeleteModelBuilderExtensions
 {
     public static ModelBuilder ApplySoftDeleteQueryFilter(this ModelBuilder modelBuilder)
     {
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            if (!typeof(ISoftDeletable).IsAssignableFrom(entityType.ClrType))
-            {
-                continue;
-            }
+        var entityTypes = modelBuilder.Model
+            .GetEntityTypes()
+            .Where(entityType => typeof(ISoftDeletable).IsAssignableFrom(entityType.ClrType))
+            .ToList();
 
+        foreach (var entityType in entityTypes)
+        {
             var param = Expression.Parameter(entityType.ClrType, "entity");
             var prop = Expression.Property(param, nameof(ISoftDeletable.IsDeleted));
             var entityNotDeleted = Expression.Lambda(Expression.Equal(prop, Expression.Constant(false)), param);
