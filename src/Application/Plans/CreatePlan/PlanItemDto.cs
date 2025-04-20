e50ca2157191cbf53dbc933d6db3cdb7330cab30
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-20 17:14:11 +0500

    Implement Quantity PlanItem

---
+++ src/Application/Plans/CreatePlan/PlanItemDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Application.Plans.CreatePlan;
+
+public class PlanItemDto
+{
+    public Guid ItemId { get; set; }
+
+    public int Quantity { get; set; }
+}
