5d6b2eef126d234cc86bd92798d43efd498451c6
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-20 15:18:19 +0500

    Fix Create Item Logic

--- src/Application/Items/CreateItem/CreateItem.cs
+++ src/Application/Items/CreateItem/CreateItem.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Common.Attributes;
 using Application.Common.Constants;
+using Application.Common.Exceptions;
 using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.External.Countries;
+using Application.Common.Interfaces.External.CurrencyExchange;
 using Application.Common.Interfaces.Repositories;
 using Domain.Entities;
 
@@ -15,8 +18,12 @@ public class CreateItemCommand(Guid clientId, CreateItemRequest item) : IRequest
 
 [RequiresClientRole(ClientRoles.Owner, ClientRoles.ClientAdmin)]
 public class CreateItemCommandHandler(
+    IItemRepository itemRepository,
+    IInvoiceRepository invoiceRepository,
+    ICustomerRepository customerRepository,
+    ICountryService countryService,
+    ICurrencyExchangeService currencyExchangeService,
     IMapper mapper,
-    IItemRepository repository,
     IValidator<CreateItemRequest> validator)
     : IRequestHandler<CreateItemCommand, ItemDto>
 {
@@ -27,7 +34,22 @@ public class CreateItemCommandHandler(
         var item = mapper.Map<Item>(request.Item);
         item.ClientId = request.ClientId;
 
-        await repository.AddAsync(item);
+        var invoice = await invoiceRepository.GetByIdAsync(item.InvoiceId)
+            ?? throw new NotFoundException($"Invoice is not found with this id: {item.InvoiceId}");
+
+        var customer = await customerRepository.GetByIdAsync(invoice.CustomerId)
+            ?? throw new NotFoundException($"Customer is not found with this id: {invoice.CustomerId}");
+
+        var customerCurrency = await countryService.GetCurrencyCodeByCountryCodeAsync(customer.CountryCode);
+        
+        var exchangedPrice = await currencyExchangeService
+            .ExchangeCurrencyAsync(item.CurrencyCode, customerCurrency, item.Price, invoice.IssueDate);
+
+        invoice.Amount += exchangedPrice;
+        
+        await invoiceRepository.UpdateAsync(invoice);
+
+        await itemRepository.AddAsync(item);
 
         return mapper.Map<ItemDto>(item);
     }
