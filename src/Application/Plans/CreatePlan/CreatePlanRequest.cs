d6d2e608339eca4484462864f77e0fb2219c62b2
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-20 16:48:06 +0500

    Fix Plan

--- src/Application/Plans/CreatePlan/CreatePlanRequest.cs
+++ src/Application/Plans/CreatePlan/CreatePlanRequest.cs
@@ -9,4 +9,6 @@ public class CreatePlanRequest
     public DateTime StartDate { get; set; }
 
     public DateTime EndDate { get; set; }
+
+    public Guid CustomerId { get; set; }
 }
--- src/Application/Plans/GetPlanById/PlanDto.cs
+++ src/Application/Plans/GetPlanById/PlanDto.cs
@@ -4,6 +4,8 @@ namespace Application.Plans.GetPlanById;
 
 public class PlanDto
 {
+    public Guid Id { get; set; }
+
     public string Title { get; set; }
 
     public DateTime StartDate { get; set; }
