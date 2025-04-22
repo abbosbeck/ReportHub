357bc86343e8aa8da36f8cabb92b5c0a305657ba
Bekzod Akhmadov <exmadovbekzod94@gmail.com>
2025-04-22 23:25:07 +0500

    AddMigrations

---
+++ src/Infrastructure/Persistence/Migrations/20250422182429_AddClientProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Clients",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Clients");
+        }
+    }
+}
