7934dc41b6fd71358696924f7c655be4be67c2e1
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-04-23 12:43:03 +0500

    Add KeyVault to Mongo Db

--- src/Infrastructure/Persistence/AppMongoDbContext.cs
+++ src/Infrastructure/Persistence/AppMongoDbContext.cs
@@ -1,19 +1,37 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
 namespace Infrastructure.Persistence;
 
-public class AppMongoDbContext(IConfiguration configuration)
+public class AppMongoDbContext(IOptions<KeyVaultOptions> keyVaultOptions)
 {
-    public IMongoDatabase Database => CreateDatabase(configuration);
+    public IMongoDatabase Database => CreateDatabase(keyVaultOptions);
 
-    public static IMongoDatabase CreateDatabase(IConfiguration configuration)
+    private static IMongoDatabase CreateDatabase(IOptions<KeyVaultOptions> keyVaultOptions)
     {
-        var connectionString = configuration.GetConnectionString("MongoDb");
+        var connectionString = GetConnectionString(keyVaultOptions);
 
         var mongoUrl = MongoUrl.Create(connectionString);
         var mongoClient = new MongoClient(mongoUrl);
 
         return mongoClient.GetDatabase("reporthub");
+    }
+
+    private static string GetConnectionString(IOptions<KeyVaultOptions> keyVaultOptions)
+    {
+        var keyVaultUrl = keyVaultOptions.Value.KeyVaultURL;
+        var keyVaultClientId = keyVaultOptions.Value.ClientId;
+        var keyVaultClientSecret = keyVaultOptions.Value.ClientSecret;
+        var keyVaultDirectoryId = keyVaultOptions.Value.DirectoryID;
+
+        var credential = new ClientSecretCredential(
+            keyVaultDirectoryId,
+            keyVaultClientId,
+            keyVaultClientSecret);
+        var client = new SecretClient(new Uri(keyVaultUrl), credential);
+
+        return client.GetSecret("MongoDb").Value.Value;
     }
 }
