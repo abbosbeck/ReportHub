9e26a3cdb618315bb6b320ed372dc4367917e712
Bekzod Akhmadov <exmadovbekzod94@gmail.com>
2025-04-25 22:07:58 +0500

    Finished

--- src/Domain/Entities/Log.cs
+++ src/Domain/Entities/Log.cs
@@ -6,25 +6,15 @@ namespace Domain.Entities;
 
 public class Log
 {
-    [BsonId]
-    [BsonElement("_id")]
-    [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid Id { get; set; }
 
-    [BsonElement("user_id")]
-    [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid UserId { get; init; }
 
-    [BsonElement("invoice_id")]
-    [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid InvoiceId { get; init; }
 
-    [BsonDateTimeOptions(Kind = DateTimeKind.Local, Representation = BsonType.String)]
     public DateTime TimeStamp { get; init; }
 
     public LogStatus Status { get; set; }
 
-    [BsonElement("client_id")]
-    [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid ClientId { get; init; }
 }
---
+++ src/Infrastructure/Persistence/Configurations/LogMappingConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public static class LogMappingConfiguration
+{
+    public static void Configure()
+    {
+        BsonClassMap.RegisterClassMap<Log>(cfg =>
+        {
+            cfg.AutoMap();
+            cfg.MapIdProperty(log => log.Id)
+            .SetElementName("_id")
+            .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+            cfg.MapProperty(log => log.UserId)
+            .SetElementName("user_id")
+            .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+            cfg.MapProperty(log => log.InvoiceId)
+            .SetElementName("invoice_id")
+            .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+            cfg.MapProperty(log => log.TimeStamp)
+            .SetSerializer(new DateTimeSerializer(DateTimeKind.Local, BsonType.String));
+
+            cfg.MapProperty(log => log.Status);
+
+            cfg.MapProperty(log => log.ClientId)
+            .SetElementName("client_id")
+            .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));
+        });
+    }
+}
