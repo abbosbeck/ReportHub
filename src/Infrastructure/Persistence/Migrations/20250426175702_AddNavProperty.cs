ea85596226ceea199d66b8549ec9096726fb287e
Umidjon <makhammadsoliyev@gmail.com>
2025-04-26 23:14:40 +0500

    Add Migration

---
+++ src/Infrastructure/Persistence/Migrations/20250426175702_AddNavProperty.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNavProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PlanItems_ItemId",
+                table: "PlanItems",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlanItems_Items_ItemId",
+                table: "PlanItems",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlanItems_Items_ItemId",
+                table: "PlanItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PlanItems_ItemId",
+                table: "PlanItems");
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20250426180722_UpdateItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Plans_Items_ItemId",
+                table: "Plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Plans_ItemId",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "Plans");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ItemId",
+                table: "Plans",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plans_ItemId",
+                table: "Plans",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Plans_Items_ItemId",
+                table: "Plans",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id");
+        }
+    }
+}
