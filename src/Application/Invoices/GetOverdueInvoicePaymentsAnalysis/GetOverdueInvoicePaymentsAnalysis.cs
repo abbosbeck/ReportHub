93e7dc3454484847c17f56929f2294916866ac92
Umidjon <makhammadsoliyev@gmail.com>
2025-04-30 20:27:23 +0500

    Add GetOverduePaymentsAnalysis UseCase

---
+++ src/Application/Invoices/GetOverdueInvoicePaymentsAnalysis/GetOverdueInvoicePaymentsAnalysis.cs
@@ -0,0 +1,60 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.External.Countries;
+using Application.Common.Interfaces.External.CurrencyExchange;
+using Application.Common.Interfaces.Repositories;
+using Application.Common.Interfaces.Time;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Invoices.GetOverdueInvoicePaymentsAnalysis;
+
+public class GetOverdueInvoicePaymentsAnalysisQuery(Guid clientId)
+    : IRequest<OverdueInvoicePaymentsAnalysisDto>, IClientRequest
+{
+    public Guid ClientId { get; set; } = clientId;
+}
+
+public class GetOverdueInvoicePaymentsAnalysisQueryHandler(
+    ICountryService countryService,
+    IDateTimeService dateTimeService,
+    IClientRepository clientRepository,
+    IInvoiceRepository invoiceRepository,
+    ICurrencyExchangeService currencyExchangeService)
+    : IRequestHandler<GetOverdueInvoicePaymentsAnalysisQuery, OverdueInvoicePaymentsAnalysisDto>
+{
+    public async Task<OverdueInvoicePaymentsAnalysisDto> Handle(
+        GetOverdueInvoicePaymentsAnalysisQuery request, CancellationToken cancellationToken)
+    {
+        var client = await clientRepository.GetByIdAsync(request.ClientId);
+        var clientCurrencyCode = await countryService.GetCurrencyCodeByCountryCodeAsync(client.CountryCode);
+        var today = dateTimeService.UtcNow.Date;
+
+        var invoices = await invoiceRepository.GetAll()
+            .Where(invoice =>
+                invoice.PaymentStatus != InvoicePaymentStatus.Paid &&
+                invoice.DueDate < today)
+            .Select(invoice => new
+            {
+                invoice.CurrencyCode,
+                invoice.Amount,
+            })
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        if (invoices.Count == 0)
+        {
+            throw new NotFoundException("No Overdue Invoice Payments are found");
+        }
+
+        var result = await Task.WhenAll(
+            invoices.Select(invoice => currencyExchangeService.ExchangeCurrencyAsync(
+                invoice.CurrencyCode, clientCurrencyCode, invoice.Amount, today)));
+
+        return new OverdueInvoicePaymentsAnalysisDto
+        {
+            CurrencyCode = clientCurrencyCode,
+            TotalAmount = result.Sum(),
+            NumberOfInvoices = result.Length,
+        };
+    }
+}
---
+++ src/Application/Invoices/GetOverdueInvoicePaymentsAnalysis/OverdueInvoicePaymentsAnalysisDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Invoices.GetOverdueInvoicePaymentsAnalysis;
+
+public class OverdueInvoicePaymentsAnalysisDto
+{
+    public int NumberOfInvoices { get; init; }
+
+    public string CurrencyCode { get; init; }
+
+    public decimal TotalAmount { get; init; }
+}
