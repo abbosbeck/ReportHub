0e04d53f6a9d6bc2fbe5ae6661c3a1836e60865d
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-03 09:26:46 +0500

    Refactor Enums

--- src/Domain/Enums/InvoicePaymentStatus.cs
+++ src/Domain/Enums/InvoicePaymentStatus.cs
@@ -2,13 +2,6 @@
 
 public enum InvoicePaymentStatus
 {
-    /// <summary>
-    /// Fully paid payment
-    /// </summary>
     Paid,
-
-    /// <summary>
-    /// Unpaid payment
-    /// </summary>
     Unpaid,
 }
--- src/Domain/Enums/LogStatus.cs
+++ src/Domain/Enums/LogStatus.cs
@@ -2,13 +2,6 @@
 
 public enum LogStatus
 {
-    /// <summary>
-    /// Success of Export
-    /// </summary>
     Success,
-
-    /// <summary>
-    /// Failure of Export
-    /// </summary>
     Failure,
 }
