b178d41e89bb31886ee4c3cb1d900ea70799031c
Umidjon <makhammadsoliyev@gmail.com>
2025-05-04 16:17:37 +0500

    Add GetCurrentUserReportSchedule UseCase

---
+++ src/Application/ReportSchedules/GetCurrentUserReportSchedule/GetCurrentUserReportSchedule.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.ReportSchedules.GetCurrentUserReportSchedule;
+
+public class GetCurrentUserReportScheduleQuery(Guid clientId)
+    : IRequest<ReportScheduleDto>, IClientRequest
+{
+    public Guid ClientId { get; set; } = clientId;
+}
+
+public class GetCurrentUserReportScheduleQueryHandler(
+    IMapper mapper,
+    ICurrentUserService service,
+    IReportScheduleRepository repository)
+    : IRequestHandler<GetCurrentUserReportScheduleQuery, ReportScheduleDto>
+{
+    public async Task<ReportScheduleDto> Handle(GetCurrentUserReportScheduleQuery request, CancellationToken cancellationToken)
+    {
+        var reportSchedule = await repository.GetByUserIdAsync(service.UserId)
+            ?? throw new NotFoundException("Report Schedule is not found");
+
+        return mapper.Map<ReportScheduleDto>(reportSchedule);
+    }
+}
