5a4525aa56371ce76544c1eff3c69f0bbda86501
Umidjon <makhammadsoliyev@gmail.com>
2025-05-04 08:53:12 +0500

    Add ReportSchedule Entity

---
+++ src/Domain/Entities/ReportSchedule.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class ReportSchedule : BaseAuditableEntity, ISoftDeletable
+{
+    public string JobKey { get; init; }
+
+    public string TriggerKey { get; init; }
+
+    public string CronExpression { get; init; }
+
+    public Guid UserId { get; init; }
+
+    public User User { get; init; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string DeletedBy { get; set; }
+}
