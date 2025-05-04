8bcef8633795dbf51ea6cefa0cf295efeef10342
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-04 20:44:59 +0500

    Send Email in a Good Manner

--- src/Infrastructure/Jobs/ReportEmailSenderJob.cs
+++ src/Infrastructure/Jobs/ReportEmailSenderJob.cs
@@ -1,18 +1,36 @@
 ﻿using Application.Common.Interfaces.Authorization;
+using Application.ExportReports.ExportReportsToFile;
+using Application.ExportReports.ExportReportsToFile.Request;
 using Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Quartz;
 
 namespace Infrastructure.Jobs;
 
-public class ReportEmailSenderJob(IEmailService emailService, UserManager<User> userManager) : IJob
+public class ReportEmailSenderJob(
+    IEmailService emailService,
+    UserManager<User> userManager,
+    IExportReportsToFileQueryHandler fileQueryHandler)
+    : IJob
 {
     public async Task Execute(IJobExecutionContext context)
     {
         var dataMap = context.MergedJobDataMap;
         var userId = dataMap.GetGuid("UserId").ToString();
         var user = await userManager.FindByIdAsync(userId);
+        var clientId = dataMap.GetGuid("ClientId");
+        var request = new ExportReportsToFileQuery(clientId, ExportReportsFileType.Excel, null);
+        var report = await fileQueryHandler.Handle(request, CancellationToken.None);
 
-        await emailService.SendEmailAsync(user!.Email, $"{user.UserName}", $"{user.FirstName}");
+        string message = $"<p><strong>Hi {user.FirstName}," +
+            $"</strong></p>\r\n\r\n<p>Your weekly report is ready and waiting for you!</p>" +
+            $"\r\n\r\n<p>\r\n<p>Have a productive week ahead!</p>" +
+            $"\r\n\r\n<p>Best regards,<br>ReportHub!</p>\r\n";
+
+        await emailService.SendEmailWithFileAsync(
+            user!.Email,
+            $"Your Weekly Report!",
+            message,
+            report.ByteArray);
     }
 }
