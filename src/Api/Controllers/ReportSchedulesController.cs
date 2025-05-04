4cb31681c17ea931b772a463e12d31dab4a55100
Umidjon <makhammadsoliyev@gmail.com>
2025-05-04 16:18:27 +0500

    Add ReportSchedules Endpoints

---
+++ src/Api/Controllers/ReportSchedulesController.cs
@@ -0,0 +1,26 @@
+﻿using Application.ReportSchedules.GetCurrentUserReportSchedule;
+using Application.ReportSchedules.ScheduleReport;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("clients/{clientId:guid}/[controller]")]
+public class ReportSchedulesController(ISender mediator) : ApiControllerBase(mediator)
+{
+    [HttpPost("schedule")]
+    public async Task<IActionResult> ScheduleAsync([FromRoute] Guid clientId, [FromBody] ScheduleReportRequest request)
+    {
+        var result = await Mediator.Send(new ScheduleReportCommand(clientId, request));
+
+        return Ok(result);
+    }
+
+    [HttpGet("me")]
+    public async Task<IActionResult> GetByUserIdAsync([FromRoute] Guid clientId)
+    {
+        var result = await Mediator.Send(new GetCurrentUserReportScheduleQuery(clientId));
+
+        return Ok(result);
+    }
+}
