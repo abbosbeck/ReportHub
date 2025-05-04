ab6cd08fd76edb16888de5c89546959d7fa0f2a8
Umidjon <makhammadsoliyev@gmail.com>
2025-05-04 16:14:47 +0500

    Add StopReportSchedule UseCase

---
+++ src/Application/ReportSchedules/StopReportSchedule/StopReportSchedule.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+
+namespace Application.ReportSchedules.StopReportSchedule;
+
+public class StopReportScheduleCommand(Guid clientId) : IRequest<bool>, IClientRequest
+{
+    public Guid ClientId { get; set; } = clientId;
+}
+
+public class StopReportScheduleCommandHandler(
+    IReportScheduleRepository repository,
+    ICurrentUserService currentUserService,
+    IReportScheduleService reportScheduleService) : IRequestHandler<StopReportScheduleCommand, bool>
+{
+    public async Task<bool> Handle(StopReportScheduleCommand request, CancellationToken cancellationToken)
+    {
+        var reportSchedule = await repository.GetByUserIdAsync(currentUserService.UserId)
+            ?? throw new NotFoundException("Report Schedule is not found");
+
+        await reportScheduleService.DeleteAsync(reportSchedule);
+        var result = await repository.DeleteAsync(reportSchedule);
+
+        return result;
+    }
+}
