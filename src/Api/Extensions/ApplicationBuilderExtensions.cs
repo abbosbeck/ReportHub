98eaef3527bbfb35727da01bcf333f10b027fa34
Umidjon <makhammadsoliyev@gmail.com>
2025-05-04 16:13:56 +0500

    Add RestoreScheduledJobsAsync

---
+++ src/Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static async Task RestoreScheduledJobsAsync(this IApplicationBuilder application)
+    {
+        using var scope = application.ApplicationServices.CreateScope();
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<AppDbContext>();
+        var service = services.GetRequiredService<IReportScheduleService>();
+        var reportSchedules = await context.ReportSchedules.ToListAsync();
+
+        await service.RestoreScheduledJobsAsync(reportSchedules);
+    }
+}
