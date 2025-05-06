8c88b6c1e340a71905f8b8a4c787f768202cedc2
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-06 15:59:51 +0500

    Implement Import Data Service

---
+++ src/Application/Common/Interfaces/Services/IImportDataFromFileService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Common.Interfaces.Services;
+
+public interface IImportDataFromFileService
+{
+    List<Customer> ImportCustomersDataFromExcel(IFormFile file);
+}
