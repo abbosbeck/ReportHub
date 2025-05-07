686c2e511c4ed7254d73045d19d1ee629524a343
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-07 09:45:21 +0500

    Add Get By Email

--- src/Application/Common/Interfaces/Repositories/IUserRepository.cs
+++ src/Application/Common/Interfaces/Repositories/IUserRepository.cs
@@ -6,6 +6,8 @@ public interface IUserRepository
 {
     Task<User> GetByIdAsync(Guid userId);
 
+    Task<User> GetByEmailAsync(string email);
+
     Task<User> AddAsync(User user);
 
     Task<User> UpdateAsync(User user);
