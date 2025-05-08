58f2a1496044e3afbaa3a3a50adeb6e972fab891
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-08 16:06:39 +0500

    Register Blazorise

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,6 +1,17 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
