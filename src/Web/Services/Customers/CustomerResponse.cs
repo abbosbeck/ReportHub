cfb4760e5fdefef765253b5ce15e18a8d2e44e26
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-09 17:08:22 +0500

    Add Customer Service

---
+++ src/Web/Services/Customers/CustomerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Services.Customers;
+
+public class CustomerResponse
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string CountryCode { get; set; }
+}
