ca6bae0cecde4cad3d07792a7617edeecaba5a29
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-09 16:12:20 +0500

    Add Get Client By User Id Handler

---
+++ src/Application/Clients/GetClientByUserId/ClientDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Clients.GetClientByUserId;
+
+public class ClientDto
+{
+    public Guid Id { get; init; }
+
+    public string Name { get; init; }
+
+    public string CountryCode { get; init; }
+}
---
+++ src/Application/Clients/GetClientByUserId/GetClientByUserId.cs
@@ -0,0 +1,35 @@
+﻿
+using Application.Common.Attributes;
+using Application.Common.Constants;
+using Application.Common.Interfaces.Authorization;
+using Application.Common.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Clients.GetClientByUserId;
+
+public class GetClientByUserIdQuery : IRequest<List<ClientDto>>
+{
+}
+
+[RequiresClientRole(ClientRoles.Owner, ClientRoles.ClientAdmin, ClientRoles.Operator)]
+public class GetClientByUserIdQueryHandler(
+    ICurrentUserService userService,
+    IClientRepository clientRepository,
+    IClientRoleAssignmentRepository clientRoleAssignmentRepository,
+    IMapper mapper)
+    : IRequestHandler<GetClientByUserIdQuery, List<ClientDto>>
+{
+    public async Task<List<ClientDto>> Handle(GetClientByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var clientRoles = await clientRoleAssignmentRepository.GetAll()
+            .Where(c => c.UserId == userService.UserId)
+            .Select(c => c.ClientId)
+            .ToListAsync(cancellationToken);
+
+        var clients = await clientRepository.GetAll()
+            .Where(c => clientRoles.Contains(c.Id))
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<List<ClientDto>>(clients);
+    }
+}
