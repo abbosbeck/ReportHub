d80d28ff32e1ef15df7c8c513d66600eacd6542b
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-10 10:18:35 +0500

    Optimise Money Service

--- src/Web/Services/ExternalServices/MoneyService.cs
+++ src/Web/Services/ExternalServices/MoneyService.cs
@@ -1,19 +1,27 @@
-﻿using System.Globalization;
+﻿using System.Collections.Concurrent;
+using System.Globalization;
 
 namespace Web.Services.ExternalServices;
 
 public class MoneyService : IMoneyService
 {
+    private static readonly ConcurrentDictionary<string, CultureInfo> _currencyCultureCache = new(StringComparer.OrdinalIgnoreCase);
+
     public string GetAmountWithSymbol(decimal amount, string currencyCode)
     {
-        var culture = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
-            .FirstOrDefault(x => new RegionInfo(x.Name).ISOCurrencySymbol.Equals(
-                currencyCode, StringComparison.OrdinalIgnoreCase));
-
-        if (culture.Name == "uz-Cyrl-UZ")
+        var culture = _currencyCultureCache.GetOrAdd(currencyCode, code =>
         {
-            culture = new CultureInfo("uz-Latn-UZ");
-        }
+            var cultureInfo = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                .FirstOrDefault(c => new RegionInfo(c.Name).ISOCurrencySymbol.Equals(
+                    code, StringComparison.OrdinalIgnoreCase));
+
+            if (cultureInfo?.Name == "uz-Cyrl-UZ")
+            {
+                return new CultureInfo("uz-Latn-UZ");
+            }
+
+            return cultureInfo ?? CultureInfo.InvariantCulture;
+        });
 
         return amount.ToString("C", culture);
     }
