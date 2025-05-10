712ff7b4551cde3fb151ae1d8ceda15e4df4a7c1
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-10 13:47:11 +0500

    Add Item Deletion & Creation

---
+++ src/Web/Models/Items/CreateItemRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Models.Items;
+
+public class CreateItemRequest
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string CurrencyCode { get; set; }
+
+    public Guid InvoiceId { get; set; }
+}
--- src/Web/Services/Items/ItemService.cs
+++ src/Web/Services/Items/ItemService.cs
@@ -1,5 +1,4 @@
 ﻿using Web.Models.Items;
-using Web.Services.Customers;
 using Web.Services.ExternalServices;
 using Web.Services.Invoices;
 
@@ -12,6 +11,28 @@ public class ItemService(
     : IItemService
 {
     private readonly HttpClient _httpClient = httpClientFactory.CreateClient("api");
+
+    public async Task<bool> CreateAsync(CreateItemRequest item, Guid clientId)
+    {
+        var respone = await _httpClient.PostAsJsonAsync($"clients/{clientId}/items", item);
+        if (respone.IsSuccessStatusCode)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id, Guid clientId)
+    {
+        var response = await _httpClient.DeleteAsync($"clients/{clientId}/items/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+
+        return false;
+    }
 
     public async Task<List<ItemResponse>> GetItemListAsync(Guid clientId)
     {
