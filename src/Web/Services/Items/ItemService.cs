fca00b81e38a735b0816b20cc0b2e42c3f483523
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-05-10 09:05:25 +0500

    Create Get Item List Service

--- src/Web/Services/Items/ItemService.cs
+++ src/Web/Services/Items/ItemService.cs
@@ -1,11 +1,33 @@
 ﻿using Web.Models.Items;
+using Web.Services.Customers;
 
 namespace Web.Services.Items;
 
-public class ItemService : IItemService
+public class ItemService(
+    IHttpClientFactory httpClientFactory,
+    ICustomerService customerService)
+    : IItemService
 {
-    public Task<List<ItemResponse>> GetItemListAsync(Guid clientId)
+    private readonly HttpClient _httpClient = httpClientFactory.CreateClient("api");
+
+    public async Task<List<ItemResponse>> GetItemListAsync(Guid clientId)
     {
-        throw new NotImplementedException();
+        var response = await _httpClient.GetAsync($"clients/{clientId}/items");
+        if (response.IsSuccessStatusCode)
+        {
+            var items = await response.Content.ReadFromJsonAsync<List<ItemResponse>>();
+
+            var tasks = items.Select(async i =>
+            {
+                var customer = await customerService.GetByIdAsync(i.CustomerId, clientId);
+                i.CustomerName = customer.Name;
+            });
+
+            await Task.WhenAll(tasks);
+
+            return items;
+        }
+
+        return new List<ItemResponse>();
     }
 }
