34acc64ebee40e98d5a787cc561f31b6250a6e1c
Umidjon <makhammadsoliyev@gmail.com>
2025-05-15 12:54:14 +0500

    Add ReportScheduleRequest.cs

---
+++ src/Web/Models/Reports/ReportScheduleRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Models.Reports;
+
+public class ReportScheduleRequest
+{
+    public string JobKey { get; init; }
+
+    public string TriggerKey { get; init; }
+
+    public string CronExpression { get; set; }
+
+    public Guid UserId { get; set; }
+    
+    public Guid ClientId { get; set; }
+}
